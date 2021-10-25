4e6765bb944a166572fc94b0a4dc93cd12940017
didwpsl <ysy183469@gmail.com>
2021-10-25 18:12:03 +0900

    hello world 출력 수정~
    삭제함 ~

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -6,7 +6,6 @@ public class HelloGit {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
 		System.out.println("hello world");
-		System.out.println("hello world");
 	}
 
 }
